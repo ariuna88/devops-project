b6d61774f316904e32d9f64c1752c5b0651ceeb8
ariuna88 <136127548+ariuna88@users.noreply.github.com>
2023-11-08 18:54:23 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+ 
+variable "environment" {
+  default = "DevOps"
+}
+ 
+variable "ec2_key" {
+  default = "pipeline1"
+}
+ 
+variable "ec2_iam_instance_profile" {
+  default = "DevOps-ariuna_2023_ec2_profile"
+}
